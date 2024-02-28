d6a090d3a5a9353aed9252871cb1a61094bd2837
sreenath <sreenathbr20@gmail.com>
2024-02-28 11:04:31 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,38 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { FormsModule } from '@angular/forms';
+import { Product } from './model/product';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+
 })
 export class AppComponent {
   title = 'my-app';
+  imageUrl = "./assets/images/fish.jpg";
+  url = "https://www.google.com";
+  isDisabled = false;
+  name = "ford";
+
+  product: Product = new Product();
+
+  public addProduct() {
+    console.log(this.product);
+  }
+  public greet() {
+    window.alert("Welcome to app");
+  }
+  public changeImage() {
+    if (this.imageUrl == "")
+      this.imageUrl = "./assets/images/fish.jpg";
+    else
+      this.imageUrl = "";
+  }
+  public displayName() {
+    console.log(this.name);
+  }
+
 }
---
+++ src/app/model/Product.ts
@@ -0,0 +1,7 @@
+export class Product{
+     public id?:number;
+     public name?:string;
+     public price?:number;
+    // constructor(id:number,name:string,price:number);
+    // constructor(){};
+}
